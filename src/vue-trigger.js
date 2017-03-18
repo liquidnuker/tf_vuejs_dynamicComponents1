271047e9ff49c9e8f80b92d985572d16c2487b81
administrator <liquidnuker@users.noreply.github.com>
2017-03-17 18:33:23 -0800

    initial commit

---
+++ src/vue-trigger.js
@@ -0,0 +1,18 @@
+// component changes when vm.currentView changes
+
+var home = {
+  template: '<p>homepage</p>'
+}
+
+var about = {
+  template: '<p>about</p>'
+}
+
+var vm = new Vue({
+  el: '#example',
+  data: {
+    currentView: home
+  }
+});
+
+vm.currentView = about;
